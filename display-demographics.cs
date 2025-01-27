57afb668eeabf51753e3a3181904e556b6432e2c
NatsuKura <66294271+natsukidxd@users.noreply.github.com>
2025-01-27 18:20:05 +0800

    Update display-demographics.cs

--- display-demographics.cs
+++ display-demographics.cs
@@ -25,23 +25,3 @@ public class display-demographics
         Console.WriteLine("Phone No.    : " + phoneNumber);
     }
 }
-
-
-
-public class FactChecker {
-
-    public static void Main(string[] args)
-    {
-        Console.Write("Enter a string: ");
-        string userString = Console.ReadLine();
-        Console.Write("Enter a integer: ");
-        int userInt = Convert.ToInt32(Console.ReadLine());
-        int stringToInt = Convert.ToInt32(userString);
-        
-        if(stringToInt == userInt){
-            Console.WriteLine("Krist Dave B. Ferrer");
-        } else {
-            Console.WriteLine("22");
-        }
-    }
-}
