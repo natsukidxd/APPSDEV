58953af2b58bfac9d869d375b2a96d35296384f1
NatsuKura <66294271+natsukidxd@users.noreply.github.com>
2025-01-27 18:18:56 +0800

    Update display-demographics.cs

--- display-demographics.cs
+++ display-demographics.cs
@@ -26,52 +26,7 @@ public class display-demographics
     }
 }
 
-// Online C# Editor for free
-// Write, Edit and Run your C# code using C# Online Compiler
 
-using System;
-
-public class Calculator
-{
-    public static void Main(string[] args)
-    {
-        string name = "Krist Dave Ferrer";
-        Console.WriteLine (name);
-        Console.WriteLine("Enter two number below.");
-        Console.Write("Num 1: ");
-        double num1 = Convert.ToDouble(Console.ReadLine());
-        Console.Write("Num 2: ");
-        double num2 = Convert.ToDouble(Console.ReadLine());
-        Console.Write("Select an operator [+ - / * % ] : ");
-        char op = Convert.ToChar(Console.ReadLine());
-        double result = 0;
-        
-        switch(op) {
-            case '+' : {
-                result = num1 + num2;
-                break;
-            }
-            case '-' : {
-                result = num1 - num2;
-                break;
-            }
-            case '/' : {
-                result = num1 / num2;
-                break;
-            }
-            case '*' : {
-                result = num1 * num2;
-                break;
-            }
-            case '%' : {
-                result = num1 % num2;
-                break;
-            }
-        }
-        
-        Console.WriteLine(num1 + " " + op + " " + num2 + " = " + result);
-    }
-}
 
 public class FactChecker {
 
